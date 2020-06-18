71bee79065a8db7e1d6168a9b1c26af538887fce
vishal turi <vishalt@ancubate.com>
2020-06-18 16:42:04 +0530

    first commit

---
+++ example-php/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once('vendor/autoload.php');
+$a = new Simple\HelloWorld();
+$a->Hello();
---
+++ simple-php/src/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+namespace Simple;
+
+class HelloWorld
+{
+    public function __construct() {
+		
+	}
+	
+	public function Hello() {
+		echo "hello function called";
+	}
+}
